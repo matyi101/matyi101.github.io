315e082d59bf2472aa2a5e5a79d7c4c5ab5a1e30
matyi101 <nazri.nawi@gmail.com>
2026-08-23 10:44:12 +0800

    update files

--- cdmp/sw.js
+++ cdmp/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cdmp-study-v13-multi-provider-auth';
+const CACHE_NAME = 'cdmp-study-v14-google-email-auth';
 const CORE = ['./', './index.html', './firebase-config.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
